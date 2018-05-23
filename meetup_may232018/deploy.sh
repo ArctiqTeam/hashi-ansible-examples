a7c12c3143585b4fca8e39f8cdf92ad1ab8bdaf8
Tim Fairweather <tim.fairweather@arctiq.ca>
2018-05-23 15:16:52 -0400

    change deploy

--- meetup_may232018/deploy.sh
+++ meetup_may232018/deploy.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 terraform apply -auto-approve
-# terraform state pull > .terraform/local.tfstate
-TF_STATE=.terraform/local.tfstate ansible-playbook -i $(which terraform-inventory) site.yml
+TF_STATE=terraform.tfstate ansible-playbook -i $(which terraform-inventory) site.yml
